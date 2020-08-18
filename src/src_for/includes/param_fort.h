f5ba2bef15fdd2163e55bbff4a192a018703865d
Yussuf Ali <Yussuf-Ali@oist.jp>
2020-08-18 11:05:15 +0900

    Added the file

---
+++ src/src_for/includes/param_fort.h
@@ -0,0 +1,27 @@
+
+!=======================================
+!
+! Addresses for integers parameters
+!
+!=======================================
+
+#define iadrHLO        1
+#define iadrNX         2
+#define iadrNY         3
+#define iadrNZ         4
+#define iadrNVAR       5
+#define iadrNVARST     6
+#define iadrNDIMTOT    7
+#define iadrNRK        8
+#define iadrNDIMPT     9
+#define iadrCacheBLCK  10
+#define iadrVARS       13
+
+
+!========================================
+!
+! Addresses for floating point parameters
+!
+!========================================
+
+#define fadrTIMESTEP 4
