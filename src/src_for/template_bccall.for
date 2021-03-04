597939cd90e85ace3067f75d453dca5b6be03549
Yussuf <Yussuf-Ali@oist.jp>
2021-03-04 14:37:04 +0900

    Modified the templates for the boundary conditions in order to compile correctly

--- src/src_for/template_bccall.for
+++ src/src_for/template_bccall.for
@@ -5,7 +5,7 @@
 !====================================================================================================
 
 subroutine boundarySchemeBCNAME(param_float,hlo,ind,idarray,neq,neqst,nx,ny,nz,sizeblck,q,qst,rhs,nvar_f,nvar_e,qface_i,qface_j,qface_k,qedge_ij,qedge_jk,qedge_ik)
-
+use iso_c_binding, only: c_float, c_double, c_int
   implicit none
 
 #include "dtypes.h"
