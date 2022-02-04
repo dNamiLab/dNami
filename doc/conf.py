a9532c6add8961f4d83c46de8acc6fe367568b6b
Yussuf <Yussuf-Ali@oist.jp>
2022-02-04 10:49:44 +0900

    Changed the thme

--- doc/conf.py
+++ doc/conf.py
@@ -29,11 +29,13 @@ author = 'Nicolas Alferez, S2T Unit'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+  "sphinx_rtd_theme",
   "sphinx.ext.autodoc",
   "sphinx.ext.coverage", 
   "sphinx.ext.napoleon",
-  "pydata_sphinx_theme"
 ]
+
+#"pydata_sphinx_theme"
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -53,8 +55,8 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store','./usage/examples.rst']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'pydata_sphinx_theme'
-#html_theme = 'sphinx_rtd_theme'
+#html_theme = 'pydata_sphinx_theme'
+html_theme = 'sphinx_rtd_theme'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
