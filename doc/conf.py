87c04026317de1653b0118518acf90d5171297d6
Yussuf <Yussuf-Ali@oist.jp>
2022-02-04 10:29:28 +0900

    Changed the config

--- doc/conf.py
+++ doc/conf.py
@@ -29,7 +29,6 @@ author = 'Nicolas Alferez, S2T Unit'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-  "sphinx_rtd_theme",
   "sphinx.ext.autodoc",
   "sphinx.ext.coverage", 
   "sphinx.ext.napoleon",
