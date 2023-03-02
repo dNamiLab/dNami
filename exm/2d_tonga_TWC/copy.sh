fd1f1c0eef4611a681558d18dc6ff338ad2aebb8
stephenwinn16 <stephen.winn@oist.jp>
2023-03-02 10:31:56 +0900

    updated the Tonga copy.sh script to copy some additional files for plotting

--- exm/2d_tonga_TWC/copy.sh
+++ exm/2d_tonga_TWC/copy.sh
@@ -7,8 +7,10 @@ mkdir $dst/bathy
 cp compute.py $dst/
 cp misc.py $dst/
 cp values.py $dst/
+cp plot.py $dst/
 
 # Copy equations and MPI modified file
 cp dnami_mpi.py ../../src/src_py
 cp equations.py ../../src/generate
 cp genRhs.py ../../src/generate
+cp functions.py ../../src/generate
